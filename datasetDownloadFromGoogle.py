b2884ef90ed81bd1e3466179ac6d01bbfb1cb64f
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-04 19:06:10 +0530

    Update datasetDownloadFromGoogle.py

--- datasetDownloadFromGoogle.py
+++ datasetDownloadFromGoogle.py
@@ -1,39 +1,5 @@
 from simple_image_download import simple_image_download as sim
 
 response = sim.simple_image_download
-
-response().download('ironman', 10)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#print(response().urls('batmanjoker', 45))
-
-#response().download('batmanjoker', 5,extensions={'.jpg'})
-#print(response().urls('batmanjoker', 5,extensions={'.jpg'}))
+response().download('ironman', 10)    # type the image you want to download 
+                                      # 10 represents the total number of images you want to download
